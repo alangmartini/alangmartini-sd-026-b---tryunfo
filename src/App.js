143e6b54d96285664279ee79d636aafe97b44bb9
gmaralan <gmartinialan@gmail.com>
2022-10-31 11:37:29 -0400

    fixed test lint

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends React.Component {
   initialState = {
     cardName: '',
     cardDescription: '',
-    cardAttr1: 0,
-    cardAttr2: 0,
-    cardAttr3: 0,
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
     cardImage: '',
     cardRare: 'normal',
     cardTrunfo: false,
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Form extends Component {
-  // constructor(props) {
-  //   super(props);
-  //  this.state = {
-
-  //  };
-  //  }
-
   render() {
     const {
       cardName,
@@ -137,9 +130,9 @@ class Form extends Component {
 Form.propTypes = {
   cardName: PropTypes.string.isRequired,
   cardDescription: PropTypes.string.isRequired,
-  cardAttr1: PropTypes.number.isRequired,
-  cardAttr2: PropTypes.number.isRequired,
-  cardAttr3: PropTypes.number.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
